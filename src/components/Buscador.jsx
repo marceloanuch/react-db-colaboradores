6cbab408a5aa94a5eefaa2e96bc787d910a20568
Marcelo Anuch <marceloanuch@me.com>
2024-03-18 15:40:26 -0300

    Modulo de Buscador completado

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,7 +1,7 @@
 const Buscador = ({ setSearch }) => {
   const buscarEnArreglo = (event) => setSearch(event.target.value)
   return (
-    
+    <input type="text" placeholder="Busca un colaborador" className="form-control col-2" onChange={buscarEnArreglo} />
   )
 }
 export default Buscador
